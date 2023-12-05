9de580ed93aadaea061cb4848339ff397eb8fe41
Fernando <Frnandocosta90@gmail.com>
2023-12-04 23:35:12 -0300

    inserindo imagens

--- main.js
+++ main.js
@@ -7,6 +7,29 @@ $('#Cancelar').click(function() {
 });
 
 $('form').on('submit', function(e){
-    console.log("submit");
     e.preventDefault();
+    const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+    console.log('Endereco da Nova Imagem:', enderecoDaNovaImagem);
+
+    const novoItem = $('<li></li>');
+
+    // Append image to the new list item
+    const novaImagem = $(`<img src="${enderecoDaNovaImagem}" />`);
+    console.log('Nova Imagem HTML:', novaImagem[0].outerHTML);
+    novaImagem.appendTo(novoItem);
+
+    // Append overlay link to the new list item
+    const overlayLink = $(
+        `<div class="overlay-imagem-link">
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+        </div>`
+    );
+    console.log('Overlay Link HTML:', overlayLink[0].outerHTML);
+    overlayLink.appendTo(novoItem);
+
+    // Append the new list item to the unordered list
+    console.log('Novo Item HTML:', novoItem[0].outerHTML);
+    novoItem.appendTo('ul');
 });
